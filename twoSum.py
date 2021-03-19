ac7d03873d6ea383f5f329e7a74e26ffd35295d8
Dolci <alessandro.dolci@axelspringer.com>
2021-03-19 21:02:55 +0100

    change logs and test with example

--- twoSum.py
+++ twoSum.py
@@ -2,21 +2,24 @@
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
+from typing import List
+import random
+
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: List[int], target: int, debug: bool) -> List[int]:
 
         solution = None
         
         # take the first of the two numbers (leave space for the second number)
         for i in range(len(nums)-1):
-            #print("i is", i)
+#             print("i is", i)
             
             # search for the second number
             for j in range(i+1, len(nums)):
-                #print("j is", j)
+#                 print("j is", j)
                 
                 if nums[i] + nums[j] == target:
-                    #print("Solution found:", nums[i], " and ", nums[j])
+#                     print("Solution found:", nums[i], " and ", nums[j])
                     solution = [i, j]
                     break
             
@@ -28,3 +31,16 @@ class Solution:
             print("Solution not found")
         else:
             return solution
+
+# create class
+s = Solution()
+
+# create input
+nums = random.sample(range(-20, 20), 20)
+print("Numbers: ", nums)
+
+target = random.randint(-40, 40)
+print("Target: ", target)
+
+sol = s.twoSum(nums, target, debug = True)
+print("The solution is: ", sol, "  --   which uses the sum of", nums[sol[0]], "and", nums[sol[1]])
